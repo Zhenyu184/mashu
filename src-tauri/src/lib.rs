3978e7e9a1d3b63feb2bb00f831dd8c7ccd5a2ea
akofan <46961059+Zhenyu184@users.noreply.github.com>
2024-10-19 17:20:29 +0800

    feat: get website information function

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,55 @@
+use std::time::Duration;
+use tokio::time::timeout;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+async fn get_web_page(url: &str) -> Result<String, String> {
+    println!("browse web pages: {}", url);
+
+    // timeout 5s
+    let response = timeout(Duration::from_secs(5), reqwest::get(url))
+        .await
+        .map_err(|_| "Request timed out".to_string())? 
+        .map_err(|e| e.to_string())?;
+
+    if response.status().is_success() {
+        let body = response.text().await.map_err(|e| e.to_string())?;
+        Ok(body)
+    } else {
+        Err(format!("Failed to fetch the page: {}", response.status()))
+    }
+}
+
+#[tauri::command]
+fn input_string(body: &str, search: &str, string: &str,) -> Result<String, String> {
+    println!("input string {}", string);
+    if true {
+        Ok(format!("logged_in {}", string))
+    } else {
+        Err("invalid credentials".to_string())
+    }
+}
+
+#[tauri::command]
+fn press_button_component(body: &str, search: &str) -> Result<String, String> {
+    println!("press {} button component", search);
+    if true {
+        Ok(format!("logged_in {}", search))
+    } else {
+        Err("invalid credentials".to_string())
+    }
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_web_page, input_string, press_button_component])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -6,31 +6,26 @@ import './App.css';
 function App() {
     const [greetMsg, setGreetMsg] = useState('');
     const [name, setName] = useState('');
+    const [url, setUrl] = useState('');
 
     async function greet() {
         // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
         setGreetMsg(await invoke('greet', { name }));
     }
 
+    async function get_web_page() {
+        const body = (await invoke('get_web_page', { url })) as string;
+        setGreetMsg(body);
+    }
+
     return (
         <main className='container'>
-            <h1>Welcome to Tauri + React</h1>
+            <h1>Welcome to Mashu</h1>
 
-            <div className='row'>
-                <a href='https://vitejs.dev' target='_blank'>
-                    <img src='/vite.svg' className='logo vite' alt='Vite logo' />
-                </a>
-                <a href='https://tauri.app' target='_blank'>
-                    <img src='/tauri.svg' className='logo tauri' alt='Tauri logo' />
-                </a>
-                <a href='https://reactjs.org' target='_blank'>
-                    <img src={reactLogo} className='logo react' alt='React logo' />
-                </a>
-            </div>
             <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
             <form
-                className='row'
+                className='row form-spacing'
                 onSubmit={(e) => {
                     e.preventDefault();
                     greet();
@@ -39,6 +34,20 @@ function App() {
                 <input id='greet-input' onChange={(e) => setName(e.currentTarget.value)} placeholder='Enter a name...' />
                 <button type='submit'>Greet</button>
             </form>
+
+            <form
+                className='row form-spacing'
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    get_web_page();
+                }}
+            >
+                <input id='greet-input' onChange={(e) => setUrl(e.currentTarget.value)} placeholder='https://v2.tauri.app/' />
+                <button type='submit'>get web page</button>
+            </form>
+
+            <button type='button' onClick={() => setGreetMsg('')}>Clear</button>
+
             <p>{greetMsg}</p>
         </main>
     );
